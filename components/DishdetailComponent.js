558d596bce0686d912682bbfe69cb9b742077b01
Andrew Christopher Wheeler <achristopherwheeler@gmail.com>
2019-09-28 11:31:36 -0700

    Redux Adding Favorites

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 import { Text, View, ScrollView, FlatList } from "react-native";
 import { Card, Icon } from "react-native-elements";
-import { DISHES } from "../shared/dishes";
-import { COMMENTS } from "../shared/comments";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import { postFavorite } from "../redux/ActionCreators";
 
 const mapStateToProps = state => {
   return {
     dishes: state.dishes,
-    comments: state.comments
+    comments: state.comments,
+    favorites: state.favorites
   };
 };
+
+const mapDispatchToProps = dispatch => ({
+  postFavorite: dishId => dispatch(postFavorite(dishId))
+});
 
 function RenderDish(props) {
   const dish = props.dish;
@@ -64,15 +68,8 @@ function RenderComments(props) {
 }
 
 class DishDetail extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      favorites: []
-    };
-  }
-
   markFavorite(dishId) {
-    this.setState({ favorites: this.state.favorites.concat(dishId) });
+    this.props.postFavorite(dishId);
   }
 
   static navigationOptions = {
@@ -86,7 +83,7 @@ class DishDetail extends Component {
       <ScrollView>
         <RenderDish
           dish={this.props.dishes.dishes[+dishId]}
-          favorite={this.state.favorites.some(el => el === dishId)}
+          favorite={this.props.favorites.some(el => el === dishId)}
           onPress={() => this.markFavorite(dishId)}
         />
         <RenderComments
@@ -99,4 +96,7 @@ class DishDetail extends Component {
   }
 }
 
-export default connect(mapStateToProps)(DishDetail);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DishDetail);

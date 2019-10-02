5a388519d996c3953848109b8931e4fa8d84a6cf
Andrew Christopher Wheeler <achristopherwheeler@gmail.com>
2019-10-02 00:07:17 -0700

    Troubleshooting

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { View, FlatList, Text } from "react-native";
 import { Tile } from "react-native-elements";
 import { connect } from "react-redux";
